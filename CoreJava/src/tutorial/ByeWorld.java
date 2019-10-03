4865cf3f78dab90ee60cbee77593515ba0a9e1aa
Jayram Rout <jayram.rout@gmail.com>
2019-10-02 20:48:59 -0400

    testing

--- CoreJava/src/tutorial/ByeWorld.java
+++ CoreJava/src/tutorial/ByeWorld.java
@@ -2,9 +2,11 @@ package tutorial;
 
 public class ByeWorld {
 
+    /**
+     * @param args
+     */
     public static void main(String[] args) {
         // TODO Auto-generated method stub
-
     }
 
 }
